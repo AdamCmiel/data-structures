f7386a71a3d1ccb7d4c860ab075c39c6d2925f74
Hack Reactor Students <adam@famo.us>
2014-01-08 15:37:37 -0800

    complete pototypal queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,5 +1,25 @@
 var makeQueue = function() {
   // Hey! Copy your code from src/functional-shared/queue.js and paste it here
+  var instance = Object.create(queueMethods);
+  instance.syze = 0;
+  return instance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue : function(value){
+    this.syze ++;
+    this[this.syze] = value;
+  },
+  dequeue : function(){
+    if (this.syze){
+      for (var i = 0; i < this.syze; i++){
+        this[i] = this[i+1];
+      }
+      this.syze--;
+      return this[0];
+    }
+  },
+  size : function(){
+    return this.syze;
+  }
+};
