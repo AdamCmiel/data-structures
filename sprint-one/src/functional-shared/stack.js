5907d0dae7ee769362efb0c3d2a174ee9d4ce91d
Hack Reactor Students <adam@famo.us>
2014-01-08 15:06:31 -0800

    complete func-shared stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,5 +1,24 @@
-var makeStack = function() {
-  // Hey! Copy your code from src/functional/stack.js and paste it here
+var makeStack = function(){
+  var instance = {
+    syze: 0
+  };
+  return _.extend(instance, stackMethods);
 };
-  
-var stackMethods = {};
+
+var stackMethods = {
+  push: function(value){
+    this.syze++;
+    this[this.syze] = value;
+  },
+  pop: function(){
+    if (this.syze){
+      var out = this[this.syze];
+      delete this[this.syze];
+      this.syze--;
+      return out;
+    }
+  },
+  size: function(){
+    return this.syze;
+  }
+};
