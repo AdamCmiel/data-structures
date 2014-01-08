1453489cd8ed16be738cf9081c7b1538b2a8d179
Hack Reactor Students <adam@famo.us>
2014-01-08 15:35:21 -0800

    complete func-shared queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,5 +1,25 @@
 var makeQueue = function(){
-  // Hey! Copy your code from src/functional/queue.js and paste it here
+  var instance = {
+    syze: 0
+  };
+  return _.extend(instance, queueMethods);
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue : function(value){
+    this.syze ++;
+    this[this.syze] = value;
+  },
+  dequeue : function(){
+    if (this.syze){
+      for (var i = 0; i < this.syze; i++){
+        this[i] = this[i+1];
+      }
+      this.syze--;
+      return this[0];
+    }
+  },
+  size : function(){
+    return this.syze;
+  }
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,3 +1,22 @@
 var Stack = function() {
-  // Hey! Copy your code from src/prototypal/stack.js and paste it here
+  this.syze = 0;
+};
+
+Stack.prototype = {
+  constructor: Stack,
+  push: function(value){
+    this.syze++;
+    this[this.syze] = value;
+  },
+  pop: function(){
+    if (this.syze){
+      var out = this[this.syze];
+      delete this[this.syze];
+      this.syze--;
+      return out;
+    }
+  },
+  size: function(){
+    return this.syze;
+  }
 };
