5c0de839224146c6b4bd8e3ab64aa85d35521852
Hack Reactor Students <adam@famo.us>
2014-01-08 15:13:34 -0800

    complete prototypal stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,5 +1,23 @@
-var makeStack = function() {
-  // Hey! Copy your code from src/functional-shared/stack.js and paste it here
+var makeStack = function(){
+  var instance  = Object.create(stackMethods);
+  instance.syze = 0;
+  return instance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value){
+    this.syze++;
+    this[this.syze] = value;
+  },
+  pop: function(){
+    if (this.syze){
+      var out = this[this.syze];
+      delete this[this.syze];
+      this.syze--;
+      return out;
+    }
+  },
+  size: function(){
+    return this.syze;
+  }
+};
