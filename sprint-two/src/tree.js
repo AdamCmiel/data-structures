08125a7b305adfd29fbf352ab7fcfeef8ccda3b6
HarleyKwyn <kwyn.meagher@gmail.com>
2014-01-09 10:28:12 -0800

    started tree problem

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,5 +1,5 @@
 var makeTree = function(value){
-  var newTree = {};
+  var newTree = Object.create(treeMethods);
   newTree.value = value;
   newTree.children = undefined;
   return newTree;
@@ -9,8 +9,25 @@ var makeTree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
+  var newTree = makeTree(value)
+  if(this.children){
+    this.children.push[newTree];
+  }else{
+    this.children = [newTree];
+  }
 };
 
 treeMethods.contains = function(target){
+  var result = false;
+  var children = this.children;
+  for(var i = 0; i<children.length; i++){
+   	if(children[i) === target){
+   		result = true;
+   	}else{
+   		result = children[i].contains(target);
+    }
+ 	}
+
+// return result;
 };
 
