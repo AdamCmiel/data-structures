83b9924362e3b74e4f2bb4a9d7a07521a1d368af
Hack Reactor Students <adam@famo.us>
2014-01-09 11:10:53 -0800

    finished set specs

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,10 +7,16 @@ var makeSet = function(){
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  this._storage || (this._storage = {});
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item){
+  return this._storage.hasOwnProperty(item);
 };
 
 setPrototype.remove = function(item){
+  if (this.contains(item)){
+    delete this._storage[item];
+  }
 };
