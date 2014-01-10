88f6503688a2f80f4df4ccc9db1c231f356e9956
HarleyKwyn <kwyn.meagher@gmail.com>
2014-01-10 12:06:14 -0800

    changed stored values to be arrays with keys. Test still failing.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -6,11 +6,19 @@ var HashTable = function(){
 HashTable.prototype = {
   insert:  function(k, v){
     var i = getIndexBelowMaxForKey(k, this._limit);
-    this._storage[i] = v;
+    var newHash = {};
+    newHash[k]=v;
+    if(this._storage[i]){
+      var subArray = [];
+      subArray.push(this._storage[i]);
+      this._storage[i] = subArray.push(newHash);
+    }else{
+      this._storage[i] = newHash;
+    }
   },
   retrieve: function(k){
     var i = getIndexBelowMaxForKey(k, this._limit);
-    return this._storage[i];
+    return this._storage[i][k];
   },
   remove: function(k){
     var i = getIndexBelowMaxForKey(k, this._limit);
