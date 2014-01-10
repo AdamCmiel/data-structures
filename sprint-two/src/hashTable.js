e08b797a71420cfa186d87d8b3ce41ef5c9d5ffe
Adam Cmiel <adamcmiel@gmail.com>
2014-01-10 12:07:28 -0800

    hash table can be resized

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -14,6 +14,36 @@ HashTable.prototype = {
   },
   remove: function(k){
     var i = getIndexBelowMaxForKey(k, this._limit);
-    this._storage[i] = null;
+  },
+  resize: function(){
+    var keyValuePairs = [];
+
+    /*key value pairs are stored in objects or arrays of objects
+    this._storage = {
+      h1: {k1, v1},
+      h2: [{k2, v2}, {k4, v4}],
+      h3: {k3, v3}
+    }
+
+    in this instance i(k2) === i(k4) and there was a collision
+    /**/
+    _.each(this._storage, function(bucket, key){
+      if (Array.isArray(bucket)){
+        _.each(bucket, function(item){
+          keyValuePairs.push(item);
+        });
+      } else {
+        keyValuePairs.push(bucket);
+      }
+    });
+    
+    var this._limit *=2;
+    this._storage = {};
+
+    _.each(keyValuePairs, function(pair){
+      var key = Object.keys(pair)[0];
+      var value = pair[key];
+      this.insert(key, value);
+    });
   }
 };
