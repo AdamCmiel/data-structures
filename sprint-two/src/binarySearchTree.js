d37eddd1ecede209e908ed1fb2c652004e8d5ac7
Adam Cmiel <adamcmiel@gmail.com>
2014-01-09 16:42:50 -0800

    attempt binary search tree insert

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,2 +1,36 @@
 var makeBinarySearchTree = function(value){
+	var newBST = {
+		right: null,
+		left : null,
+		value: value
+	};
+	return _.extend(newBST, binarySearchTreeMethods);
+};
+
+var binarySearchTreeMethods = {
+	insert: 		 function(value) {
+		var newNode = makeBinarySearchTree(value);
+		//debugger;
+		if (this && this.value){
+			if (value > this.value) {
+				this.right ? this.right.insert(newNode) : (this.right = newNode);
+			} else if (value < this.value) {
+				this.left  ? this.left .insert(newNode) : (this.left  = newNode);
+			}
+		} else {
+			this.value = value;
+		}
+	},
+	contains: 		 function(value) {
+		
+	},
+	traverse:        function(callback) {
+
+	},
+	depthFirstLog:   function(callback) {
+
+	},
+	breadthFirstLog: function(callback) {
+
+	}
 };
